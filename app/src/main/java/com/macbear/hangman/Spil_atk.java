01e6abbbf9c80304b1efe64e1015b8c04d11810b
Bjqrn88 <andersbjorn88@gmail.com>
2016-02-11 15:35:28 +0100

    føtste

---
+++ app/src/main/java/com/macbear/hangman/Spil_atk.java
@@ -0,0 +1,13 @@
+package com.macbear.hangman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Spil_atk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spil);
+    }
+}
